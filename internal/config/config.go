516e1521efb1669aad455d90a07b58e6329fd3c3
ligege076 <2908792545@qq.com>
2026-04-03 01:07:46 +0800

    init shortener project

---
+++ internal/config/config.go
@@ -0,0 +1,20 @@
+// Code scaffolded by goctl. Safe to edit.
+// goctl 1.10.1
+
+package config
+
+import "github.com/zeromicro/go-zero/rest"
+
+type ShortUrlDB struct {
+	DSN string
+}
+
+type Config struct {
+	rest.RestConf
+
+	ShortUrlDB ShortUrlDB
+
+	Sequence struct {
+		DSN string
+	}
+}
---
+++ internal/logic/showlogic.go
@@ -0,0 +1,33 @@
+// Code scaffolded by goctl. Safe to edit.
+// goctl 1.10.1
+
+package logic
+
+import (
+	"context"
+
+	"shortener/internal/svc"
+	"shortener/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type ShowLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewShowLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ShowLogic {
+	return &ShowLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *ShowLogic) Show(req *types.ShowRequest) (resp *types.ShowResponse, err error) {
+	// todo: add your logic here and delete this line
+
+	return
+}
